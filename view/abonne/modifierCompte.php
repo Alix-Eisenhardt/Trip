92fcdb19bd774b43bafa9059bb85e2f4448b0534
Alix VUAGNOUX <vuagnoua@iut-acy.local>
2016-09-23 15:40:47 +0200

    Ajout du formulaire de modification de compte. Code non fini.

---
+++ view/abonne/modifierCompte.php
@@ -0,0 +1,42 @@
+ <form action="?r" method="post">
+	<h1>Mon compte</h1>
+
+	<p>
+		<label for="adresse1">Adresse</label>
+		<input type="text" id="adresse1" name="abo_adrligne1">
+	</p>
+	<p>
+		<label for="adresse2">Complement</label>
+		<input type="text" id="adresse2" name="abo_adrligne2">
+	</p>
+	<p>
+		<label for="cp">Code postal</label>
+		<input type="text" id="cp" name="abo_cp">
+	</p>
+	<p>
+		<label for="ville">Ville</label>
+		<input type="text" id="ville" name="abo_ville">
+	</p>
+	<p>
+		<label for="etat">Etat</label>
+		<input type="text" id="etat" name="abo_etat">
+	</p>
+	<p>
+		<label for="pays">Pays</label>
+		<select id="pays" name="pay_id">
+			
+		</select>
+	</p>
+	<p>
+		<label for="adresse1">Indicatif</label>
+		<input type="text" id="adresse1" name="abo_indicatif">
+	</p>
+	<p>
+		<label for="adresse1">Telephone</label>
+		<input type="text" id="adresse1" name="abo_tel">
+	</p>
+
+	<p class="button">
+		<button type="submit">Valider</button>
+	</p>
+</form>
