a70a5b9be2c6f6d4ca4b88936b4102a916013e5a
Lescurel <louis.klein7@gmail.com>
2016-09-25 22:04:13 +0200

    Changing name of input form to standard in the db

--- view/location/createLocation.php
+++ view/location/createLocation.php
@@ -5,7 +5,7 @@
 
   <p>
     <label for="name">Nom de la location:</label>
-    <input type="text" id="name" name="loc_name" value="<?php if (isset ($_POST['loc_name'])) echo htmlentities(trim($_POST['loc_name'])); ?>">
+    <input type="text" id="name" name="loc_titre" value="<?php if (isset ($_POST['loc_titre'])) echo htmlentities(trim($_POST['loc_titre'])); ?>">
   </p>
 
   <p>
@@ -15,12 +15,12 @@
 
   <p>
     <label for="Description">Description de la location</label>
-    <textarea id="descr" name="loc_descr"></textarea>
+    <textarea id="descr" name="loc_description"></textarea>
   </p>
 
   <p>
     <label for="nbChambres">Nombre de Chambres</label>
-    <input type="number" id="nbroom" name="loc_nbroom" value="<?php if (isset ($_POST['loc_nbroom'])) echo htmlentities(trim($_POST['loc_nbroom'])); ?>">
+    <input type="number" id="nbroom" name="loc_nbchambres" value="<?php if (isset ($_POST['loc_nbchambres'])) echo htmlentities(trim($_POST['loc_nbchambres'])); ?>">
   </p>
 
   <p>
@@ -30,22 +30,22 @@
 
   <p>
     <label for="nbBathroom">Nombre de Salles de Bains</label>
-    <input type="number" id="nbbathroom" name="loc_nbbathroom" value="<?php if (isset ($_POST['loc_nbbathroom'])) echo htmlentities(trim($_POST['loc_nbbathroom'])); ?>">
+    <input type="number" id="nbbathroom" name="loc_nbsallesbain" value="<?php if (isset ($_POST['loc_nbsallesbain'])) echo htmlentities(trim($_POST['loc_nbsallesbain'])); ?>">
   </p>
 
   <p>
     <label for="nbOccupants">Nombre d'occupants Maximum</label>
-    <input type="number" id="nbOccupants" name="loc_nbOccupants" value = "<?php if (isset ($_POST['loc_nbOccupants'])) echo htmlentities(trim($_POST['loc_nbOccupants'])); ?>">
+    <input type="number" id="nbOccupants" name="loc_nboccupants" value = "<?php if (isset ($_POST['loc_nboccupants'])) echo htmlentities(trim($_POST['loc_nboccupants'])); ?>">
   </p>
 
   <p>
     <label for="addr">Adresse :</label>
-    <input type="text" id="addr" name="loc_addr" value="<?php if (isset ($_POST['loc_addr'])) echo htmlentities(trim($_POST['loc_addr'])); ?>">
+    <input type="text" id="addr" name="loc_adrligne1" value="<?php if (isset ($_POST['loc_adrligne1'])) echo htmlentities(trim($_POST['loc_adrligne1'])); ?>">
   </p>
 
   <p>
     <label for="addr2">Complement d'adresse :</label>
-    <input type="text" id="addr2" name="loc_addr2" value ="<?php if (isset ($_POST['loc_addr2'])) echo htmlentities(trim($_POST['loc_addr2'])); ?>">
+    <input type="text" id="addr2" name="loc_adrligne2" value ="<?php if (isset ($_POST['loc_adrligne2'])) echo htmlentities(trim($_POST['loc_adrligne2'])); ?>">
   </p>
 
   <p>
@@ -55,17 +55,17 @@
 
   <p>
     <label for="city">Ville</label>
-    <input type="text" id="addr" name="loc_city" value ="<?php if (isset ($_POST['loc_city'])) echo htmlentities(trim($_POST['loc_city'])); ?>">
+    <input type="text" id="addr" name="loc_ville" value ="<?php if (isset ($_POST['loc_ville'])) echo htmlentities(trim($_POST['loc_ville'])); ?>">
   </p>
 
   <p>
     <label for="state">Etat</label>
-    <input type="text" id="state" name="loc_state" value="<?php if (isset ($_POST['loc_state'])) echo htmlentities(trim($_POST['loc_state'])); ?>">
+    <input type="text" id="state" name="loc_etat" value="<?php if (isset ($_POST['loc_etat'])) echo htmlentities(trim($_POST['loc_etat'])); ?>">
   </p>
 
   <p>
     <label for="country">Pays :</label>
-    <select>
+    <select id="pays" name="pay_id">
       <?php
        foreach (Pays::findAll() as $v) {
           echo "<option value = \"$v\"> $v </option>";
