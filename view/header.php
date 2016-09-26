b941487b905af3af7ca1e1e8e03705b5677bcb78
Alix VUAGNOUX <vuagnoua@iut-acy.local>
2016-09-26 14:16:01 +0200

    annulation des modifications sur header.php

--- view/header.php
+++ view/header.php
@@ -10,12 +10,8 @@
 		<h1>TripAdvisor</h1>
 		<ul>
 			<a href="."><li>Accueil</li></a>
-			<?php
-			if(isset($_SESSION['ouvert']))
-				echo "<a href=\"./?r=abonne/modifierCompte\"><li>Mon compte</li></a>";
-			else
-				echo "<a href=\"./?r=abonne/inscription\"><li>Inscription</li></a>";
-			?>
+			<a href="./?r=abonne/modifierCompte"><li>Mon compte</li></a>
+			<a href="./?r=abonne/inscription"><li>Inscription</li></a>			
 			<a href="./?r=location/createLocation"><li>Ajouter location</li></a>
 		</ul>
 </header>
