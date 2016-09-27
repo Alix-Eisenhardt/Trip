3171ba9abbe1402e04072f1b56765f30d30addae
Pierre VARLEZ <pierre.varlez@gmail.com>
2016-09-27 17:23:45 +0200

    Search by date

--- view/location/search.php
+++ view/location/search.php
@@ -1,11 +1,11 @@
 <?php
 global $data;
-if($data == null) {
+if($data[1] == null) {
 	echo "Aucune location trouvée pour ces critères de recherche.";
 } else {?>
-	<h1>Locations près de <?php echo $data[0]->loc_ville ?></h1>
+	<h1>Locations près de <?php echo $data[0] ?></h1>
 	<?php
-	foreach ($data as $key => $value) {
+	foreach ($data[1] as $key => $value) {
 		echo $value->shortVersion();
 	};
 }
