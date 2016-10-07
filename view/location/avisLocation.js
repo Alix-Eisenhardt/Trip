869b94a42e1d09ed6384c1c28c90481c52f18b02
Pierre VARLEZ <pierre.varlez@gmail.com>
2016-10-07 14:28:41 +0200

    Implemented sort by note

--- view/location/avisLocation.js
+++ view/location/avisLocation.js
@@ -16,14 +16,27 @@ $(document).ready(function() {
 				.append("<option>Français</option>")
 				.append("<option>English</option>");
 
-	$('#avis').prepend(tri);
-	$('#avis').prepend(langue);
+	$(tri).insertBefore('#avis');
+	$(langue).insertBefore('#avis');
 
 	triSelect.click(function() {
 		if(triSelect.val() == "Date (du + vieux au + récent)") {
-			$.makeArray($(".date"));
+			var avis = $.makeArray($(".avis"));
+		} else if(triSelect.val() == "Date (du + récent au plus vieux)") {
+			var avis = $.makeArray($(".avis"));
+		} else if(triSelect.val() == "Avis (du moins bon au meilleur)") {
+			var avis = $(".avis");
+			avis.sort(function(a, b) {
+				return $(a).find(".note").text() - $(b).find(".note").text();
+			});
+			$("#avis").html(avis);
+		} else if(triSelect.val() == "Avis (du meilleur au moins bon)") {
+			var avis = $(".avis");
+			avis.sort(function(a, b) {
+				return $(b).find(".note").text() - $(a).find(".note").text();
+			});
+			$("#avis").html(avis);
 		}
-		console.log(triSelect.val());
 	});
 
 	langueSelect.click(function() {
