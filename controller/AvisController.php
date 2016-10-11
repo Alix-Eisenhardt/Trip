2fe069e918d0744850e6f4526b02b9a408ab233b
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-11 09:45:29 +0200

    problème de merge sur aviscontroller

--- controller/AvisController.php
+++ controller/AvisController.php
@@ -1,6 +1,7 @@
 <?php
 class AvisController extends Controller {
 	public function creation() {
+		global $erreur;
 		if (isset($_POST['envoyer']) && $_POST['envoyer'] == 'Envoyer') {
 			if ((isset($_POST['vil']) && !empty($_POST['vil'])) 
 				&&(isset($_POST['per']) && !empty($_POST['per']))
@@ -15,23 +16,48 @@ class AvisController extends Controller {
 						$_POST['recom'] = "TRUE";
 					}
 
-					$param = array(
-						"abo_id"=> $_SESSION['abo']->abo_id,
-						"loc_id"=> $_GET['loc_id'],
-						"vil_id"=> $_POST['vil'],
-						"per_id"=> $_POST['per'],
-						"lng_id"=> $_POST['lng'],
-						"avi_nomoccupant"=> $_POST['nom_occ'],
-						"avi_titre"=> $_POST['titre'],
-						"avi_detail"=> $_POST['detail'],
-						"avi_noteglobale"=> $_POST['note_glob'],
-						"avi_recommandationami"=> $_POST['recom'],
-					);
-					$avis = new Avis($param);
-					$mail_to = $_SESSION['abo']->abo_mel;
-					melConfirm($mail_to);
-					$path = "index.php?r=location/showLocation&id=".$_GET['loc_id'];
-					header("Location: $path");
+					//tri des reponses facultatives
+					foreach ($_POST as $key => $value) {
+						if(preg_match('/^quest/',$key) && !empty($value))
+							$questions_facultatives[substr($key,5)] = $value;
+					}
+					if(isset($questions_facultatives)) {
+						if(count($questions_facultatives) != $_POST['count_questions'])
+							$erreur = "Vous n'avez pas rempli toutes les questions facultatives";
+					}
+
+					//abonne
+					if(!isset($erreur)) {
+						$param = array(
+							"abo_id"=> $_SESSION['abo']->abo_id,
+							"loc_id"=> $_GET['loc_id'],
+							"vil_id"=> $_POST['vil'],
+							"per_id"=> $_POST['per'],
+							"lng_id"=> $_POST['lng'],
+							"avi_nomoccupant"=> $_POST['nom_occ'],
+							"avi_titre"=> $_POST['titre'],
+							"avi_detail"=> $_POST['detail'],
+							"avi_noteglobale"=> $_POST['note_glob'],
+							"avi_recommandationami"=> $_POST['recom'],
+						);
+						$avis = new Avis($param);
+						$mail_to = $_SESSION['abo']->abo_mel;
+						melConfirm($mail_to);
+
+						//insertion des reponses
+						if(isset($questions_facultatives)) {
+							foreach ($questions_facultatives as $key => $value) {
+							$param = array(
+								'avi_id' => $avis->avi_id,
+								'qul_id' => $key,
+								'rel_reponse' => $value 
+								);
+							$rel = new Reponse($param);
+						}
+						//redirection
+						$path = "index.php?r=location/showLocation&id=".$_GET['loc_id'];
+						header("Location: $path");
+					}
 				} else 
 					$erreur = "l'un des champs n'est pas renseigné"; 
 			}
@@ -39,11 +65,6 @@ class AvisController extends Controller {
 			$this->render("creation");
 		else
 			$this->render("non_connecte");
-		if(isset($erreur)) {
-			echo $erreur;
-			unset($erreur);
-		}
 	}
-}   /*
-	protected $_avi_reponseavis;
-	*/
+	}
+}
