0bb5a7d605f729aea36318da3bf93951aa06e8a1
Pierre VARLEZ <pierre.varlez@gmail.com>
2016-10-11 09:52:48 +0200

    Adding images

--- view/location/showLocation.php
+++ view/location/showLocation.php
@@ -3,6 +3,18 @@ global $data;
 // Affichage de la location
 echo $data[0];
 
+$photos = Photo::findAll();
+
+echo "<script type=\"text/javascript\" src='view/location/showImages.js'></script>";
+
+foreach ($photos as $photo) {
+	if($photo->loc_id == $data[0]->loc_id) {
+		echo "<div class='photo'>";
+		echo "<img src='../upload/".$photo->pho_url."'></img>";
+		echo "</div>";
+	}
+}
+
 $path = "./?r=avis/creation&loc_id=".$data[0]->loc_id;
 echo "<a class='button' href='$path'>Ecrire un Avis</a>";
 
