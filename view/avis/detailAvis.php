5d2562dd9991a1377886b81394f2da0de59a338d
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-14 16:12:20 +0200

    fix affichage des reponses aux questions fac

---
+++ view/avis/detailAvis.php
@@ -0,0 +1,10 @@
+<?php
+global $data;
+echo $data['avis'];
+if(!empty($data['questions']))
+{
+	echo "<h4>Reponses aux questions facultatives </h4>";
+	foreach ($data['questions'] as $key => $value) {
+		echo $value;
+	}
+}
