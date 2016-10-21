38d97903b19f4daa55d0ab67edd73632c08c67a1
Pierre VARLEZ <pierre.varlez@gmail.com>
2016-10-21 13:24:56 +0200

    Adding error messages on date check

--- view/site/index.php
+++ view/site/index.php
@@ -3,11 +3,11 @@
 	<label for="ville">Ville : </label>
 	<input type="text" id="ville" name="ville" required/>
 	</p>
-	<p>
+	<p id="dateDebut">
 	<label for="date_debut">Date début : </label>
 	<input type="date" id="date_debut" name="date_debut"/>
 	</p>
-	<p>
+	<p id="dateFin">
 	<label for="date_fin">Date fin : </label>
 	<input type="date" id="date_fin" name="date_fin"/>
 	</p>
--- view/tools.js
+++ view/tools.js
@@ -1,16 +1,42 @@
 var today = new Date();
-	today.setHours(0,0,0,0);
+today.setHours(0,0,0,0);
+var msg = $("<p/>");
+msg.attr("id", "erreur");
 
-	function checkDates() {
-		var dateDebut = document.getElementById('date_debut');
-		var dateFin = document.getElementById('date_fin');
-		var dDebut = new Date(dateDebut.value);
-		var dFin = new Date(dateFin.value);
-		if(!dateDebut.value && dateFin.value ||
-			dateDebut.value && !dateFin.value ||
-			dDebut < today || dFin < today || dFin < dDebut) {
-			return false;
+function checkDates() {
+	var dateDebut = document.getElementById('date_debut');
+	var dateFin = document.getElementById('date_fin');
+	var dDebut = new Date(dateDebut.value);
+	var dFin = new Date(dateFin.value);
+	if(!dateDebut.value && dateFin.value ||
+		dateDebut.value && !dateFin.value ||
+		dDebut < today ||
+		dFin < today ||
+		dFin < dDebut
+	) {
+		if(!dateDebut.value && dateFin.value) {
+			console.log("test dd vide");
+			msg.text("Il faut rentrer une date de début");
+			$("#dateDebut").append(msg);
+		} else if(dateDebut.value && !dateFin.value) {
+			console.log("test df vide");
+			msg.text("Il faut rentrer une date de fin");
+			$("#dateFin").append(msg);
+		} else if(dDebut < today) {
+			console.log("test");
+			msg.text("La date de début doit être après aujourd'hui");
+			$("#dateDebut").append(msg);
+		} else if(dFin < today) {
+			console.log("test");
+			msg.text("La date de fin doit être après aujourd'hui");
+			$("#dateFin").append(msg);
 		} else {
-			return true;
+			console.log("test");
+			msg.text("La date de fin doit être après celle de début");
+			$("#dateFin").append(msg);
 		}
+		return false;
+	} else {
+		return true;
 	}
+}
