02bd321962c4b0eb29522f77916016daaf59a4ce
Louis KLEIN <louis.klein7@gmail.com>
2016-10-21 13:16:17 +0200

    Commentaire pour la calendrier

--- view/location/dispo.js
+++ view/location/dispo.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 	$("[id^=date_]").focus(function(){
        $(this).blur();
 	});
-
+	//On recupère la date du jour et on la split
 	var d = new Date();
 	var y = d.getFullYear();
 	var m = d.getMonth()+1;
 	var dd = d.getDate();
 	console.log(y+"-"+m+"-"+dd);
+	//Pour chaque id contenant la date, on affecte la couleur verte et une fonction permettant d'insérer la date dans un input box 
+	//Traitement un peu particulier pour savoir s'il faut inserer la valeur dans le textbox de début ou de fin. 
 	for(var i=0;i<day.length;i++){
 		(function(i){
 		var date = day[i].split("-");
@@ -18,17 +20,16 @@ $(document).ready(function(){
 			$('#'+day[i]).css({"color":"green", "background-color":"#00fa00", "cursor":"pointer"});
 
 			$("#"+day[i]).click(function(){
-
+				//Si la date de début est vide, on la remplit
 				if($('#date_debut_resa').val() == ''){
-					console.log(day[i]);
 					$('#date_debut_resa').val(date[2]+"/"+date[1]+"/"+date[0]);
 				}else{
 					var dateDebut = $('#date_debut_resa').val().split("/");
-					console.log(dd);
+					//Si la date est plus petite que la date de début, on remplace la date de début
 					if(dateDebut[1]>= date[1] && dateDebut[0] >= date[2] && dateDebut[2] >= date[0])
 						$('#date_debut_resa').val(date[2]+"/"+date[1]+"/"+date[0]);
 					else{
-						console.log("fin");
+						//on remplit la date de fin sinon.
 						$('#date_fin_resa').val(date[2]+"/"+date[1]+"/"+date[0]);
 					}
 				}
@@ -36,12 +37,12 @@ $(document).ready(function(){
 		}
 	})(i);
 	}
-
+	//On affecte au bouton reset une fonction vidant les textbox des dates
 	$('#reset').click(function(){
 		$("[id^=date_]").val("");
 	});
 });
-
+//Fonction de vérification du formulaire
 function checkTxt(){
 	var dateDebut = document.getElementById('date_debut_resa');
 	var dateFin = document.getElementById('date_fin_resa');
